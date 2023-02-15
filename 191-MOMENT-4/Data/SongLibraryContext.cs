cc225611c1f78722a363dd7d613723b36c43e2c4
DamjanT1988 <Damjan_t@hotmail.com>
2023-02-15 13:43:18 +0100

    1

---
+++ 191-MOMENT-4/Data/SongLibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using _191_MOMENT_4.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace _191_MOMENT_4.Data
+{
+    public class SongLibraryContext : DbContext
+    {
+        //constructor
+        //store in variable options
+        public SongLibraryContext(DbContextOptions<SongLibraryContext> options) : base(options)
+        {
+
+        }
+
+        //connect to model
+        public DbSet<SongModel> SongLibrary { get; set; }
+    }
+}
--- 191-MOMENT-4/Program.cs
+++ 191-MOMENT-4/Program.cs
@@ -1,3 +1,6 @@
+using _191_MOMENT_4.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//connect to db
+builder.Services.AddDbContext<SongLibraryContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbString")));
 
 var app = builder.Build();
 
