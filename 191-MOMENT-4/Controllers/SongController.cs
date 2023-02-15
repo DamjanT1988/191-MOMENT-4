f3ca945f8e1979df38440a02093f67d50155a5e0
DamjanT1988 <Damjan_t@hotmail.com>
2023-02-15 14:01:03 +0100

    1

---
+++ 191-MOMENT-4/Controllers/SongController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using _191_MOMENT_4.Data;
+using _191_MOMENT_4.Model;
+
+namespace _191_MOMENT_4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private readonly SongLibraryContext _context;
+
+        public SongController(SongLibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Song
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SongModel>>> GetSongLibrary()
+        {
+            return await _context.SongLibrary.ToListAsync();
+        }
+
+        // GET: api/Song/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SongModel>> GetSongModel(int id)
+        {
+            var songModel = await _context.SongLibrary.FindAsync(id);
+
+            if (songModel == null)
+            {
+                return NotFound();
+            }
+
+            return songModel;
+        }
+
+        // PUT: api/Song/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSongModel(int id, SongModel songModel)
+        {
+            if (id != songModel.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(songModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SongModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Song
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SongModel>> PostSongModel(SongModel songModel)
+        {
+            _context.SongLibrary.Add(songModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSongModel", new { id = songModel.ID }, songModel);
+        }
+
+        // DELETE: api/Song/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSongModel(int id)
+        {
+            var songModel = await _context.SongLibrary.FindAsync(id);
+            if (songModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.SongLibrary.Remove(songModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SongModelExists(int id)
+        {
+            return _context.SongLibrary.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ 191-MOMENT-4/Migrations/20230215125437_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _191_MOMENT_4.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SongLibrary",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Artist = table.Column<string>(type: "TEXT", nullable: true),
+                    SongTitle = table.Column<string>(type: "TEXT", nullable: true),
+                    LengthSec = table.Column<int>(type: "INTEGER", nullable: true),
+                    Category = table.Column<string>(type: "TEXT", nullable: true),
+                    YearPub = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SongLibrary", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SongLibrary");
+        }
+    }
+}
