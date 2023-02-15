3c7e539cbb3cc0d5b7d6a5e80a5ee7a014363a9c
DamjanT1988 <Damjan_t@hotmail.com>
2023-02-15 13:01:56 +0100

    Add project files.

---
+++ 191-MOMENT-4/Model/SongModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _191_MOMENT_4.Model
+{
+    public class SongModel
+    {
+        public SongModel()
+        {
+
+
+        }
+    }
+}
