60081ace2a58c8be232afe2f760ff5b61f159593
DamjanT1988 <Damjan_t@hotmail.com>
2023-02-16 07:33:47 +0100

    1

---
+++ 191-MOMENT-4/Migrations/AlbumLibrary/20230216062128_SecondDB.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _191_MOMENT_4.Migrations.AlbumLibrary
+{
+    /// <inheritdoc />
+    public partial class SecondDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AlbumLibrary",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Artists = table.Column<string>(type: "TEXT", nullable: true),
+                    SongAmount = table.Column<int>(type: "INTEGER", nullable: true),
+                    TotLengthSec = table.Column<int>(type: "INTEGER", nullable: true),
+                    Cathegory = table.Column<string>(type: "TEXT", nullable: true),
+                    YearPub = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlbumLibrary", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlbumLibrary");
+        }
+    }
+}
